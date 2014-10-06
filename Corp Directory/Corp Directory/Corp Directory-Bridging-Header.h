bed9036fae76fe78d7d66f63471f4bcde3cb41ad
Rich Randall <rrandall@microsoft.com>
2014-10-06 16:39:48 -0700

    Add dependencies before moving code over from prototype.

---
+++ Corp Directory/Corp Directory/Corp Directory-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import "RestKit/RestKit.h"
+#import "SDWebImage/UIImageView+WebCache.h"
+#import "ADALiOS/ADAuthenticationContext.h"
