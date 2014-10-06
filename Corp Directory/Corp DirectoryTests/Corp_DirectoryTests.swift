8bf8d0f9bc953f82ca8e1706602d255c0b026309
Rich Randall <rrandall@microsoft.com>
2014-10-06 15:51:50 -0700

    Base project template

---
+++ Corp Directory/Corp DirectoryTests/Corp_DirectoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Corp_DirectoryTests.swift
+//  Corp DirectoryTests
+//
+//  Created by Rich Randall on 10/6/14.
+//  Copyright (c) 2014 Rich Randall. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Corp_DirectoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
